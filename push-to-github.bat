95f180c5920cf25cb9ad575f8e0f2de97d7d2f56
NileshHake <nbhbiotech.hake@gmail.com>
2026-09-09 11:47:05 +0530

    Add push-to-github.bat script

---
+++ push-to-github.bat
@@ -0,0 +1,13 @@
+@echo off
+title Push CarelioEMR to GitHub
+cd /d "%~dp0"
+echo ====================================================
+echo   Pushing CarelioEMR to GitHub (main branch)
+echo ====================================================
+echo.
+git push -u origin main
+echo.
+echo ====================================================
+echo   Process finished. Press any key to close this window.
+echo ====================================================
+pause
