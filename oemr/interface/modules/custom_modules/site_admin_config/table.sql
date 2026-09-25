6f4f9501213af5c4e6af6c2f751007fa06c0a791
NileshHake <nbhbiotech.hake@gmail.com>
2026-09-25 15:08:12 +0530

    updates

--- oemr/interface/modules/custom_modules/site_admin_config/table.sql
+++ oemr/interface/modules/custom_modules/site_admin_config/table.sql
@@ -236,8 +236,8 @@ WHERE `form_id` = 'DEM'
 -- ============================================================================
 UPDATE `layout_options` SET `data_type` = 26, `list_id` = 'country', `uor` = 1, `seq` = 4, `title` = 'Country' WHERE `form_id` = 'DEM' AND `field_id` = 'country_code';
 UPDATE `layout_options` SET `data_type` = 26, `list_id` = 'country', `uor` = 1, `seq` = 4, `title` = 'Country' WHERE `form_id` = 'DEM' AND `field_id` = 'country';
-UPDATE `layout_options` SET `data_type` = 26, `list_id` = 'state', `uor` = 1, `seq` = 5, `title` = 'State' WHERE `form_id` = 'DEM' AND `field_id` = 'state';
-UPDATE `layout_options` SET `data_type` = 26, `list_id` = 'county', `uor` = 1, `seq` = 6, `title` = 'County' WHERE `form_id` = 'DEM' AND `field_id` = 'county';
+UPDATE `layout_options` SET `data_type` = 26, `list_id` = 'state', `uor` = 1, `seq` = 5, `title` = 'State / Parish / District' WHERE `form_id` = 'DEM' AND `field_id` = 'state';
+UPDATE `layout_options` SET `data_type` = 26, `list_id` = 'county', `uor` = 1, `seq` = 6, `title` = 'County / Community' WHERE `form_id` = 'DEM' AND `field_id` = 'county';
 UPDATE `layout_options` SET `seq` = 7 WHERE `form_id` = 'DEM' AND `field_id` = 'postal_code';
 
 -- ============================================================================
